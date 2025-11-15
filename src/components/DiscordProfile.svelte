b1e4fa4dba3fa7246eb86295e187e95431d4dcb0
screepai <scr33py294@gmail.com>
2025-11-15 11:59:16 +0700

    force gradient

--- src/components/DiscordProfile.svelte
+++ src/components/DiscordProfile.svelte
@@ -20,7 +20,8 @@
       const useDisplayName = "true";
       const waveColor = isDark ? "bfc5e7-light" : "f3bdbb";
       const gradient = isDark ? "bfc5e7-a9b0e6-868ac9-6065da" : "f3bdbb-f7a9a7-e98e8c-fe7585";
-      return `https://lanyard.kyrie25.dev/api/534375062099460097?theme=${theme}&bg=${bg}&idleMessage=${encodeURIComponent(idleMessage)}&hideBadges=${hideBadges}&hideNameplate=${hideNameplate}&useDisplayName=${useDisplayName}&waveColor=${waveColor}&waveSpotifyColor=${waveColor}&gradient=${gradient}`;
+      const forceGradient = "true";
+      return `https://lanyard.kyrie25.dev/api/534375062099460097?theme=${theme}&bg=${bg}&idleMessage=${encodeURIComponent(idleMessage)}&hideBadges=${hideBadges}&hideNameplate=${hideNameplate}&useDisplayName=${useDisplayName}&waveColor=${waveColor}&waveSpotifyColor=${waveColor}&gradient=${gradient}&forceGradient=${forceGradient}`;
    }
 
    $: newDiscordUrl = getDiscordUrl(darkMode);
